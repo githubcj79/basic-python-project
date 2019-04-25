d383b12fbfa3d08ca15248c96beab2d6bb61a876
carlos <carlos.jimenez@acepta.com>
2019-04-25 11:43:41 -0400

    Logging: incorporando snk_logging module.

--- basicp/app.py
+++ basicp/app.py
@@ -4,9 +4,15 @@
 
 from __future__ import print_function, with_statement
 
+from basicp.common.snk_logging import get_logger
+
+logger = get_logger(__name__)
+
+'''
 from basicp.common.my_logging import logger
 
 logger.name = __name__
+'''
 
 def run():
     print('Hello world !!!')
